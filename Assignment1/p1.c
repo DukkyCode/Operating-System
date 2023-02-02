8d44a86d052dd7f94b57115d21d8891821523cc1
Duc Nguyen <ducanhnguyen_official@outlook.com>
2023-02-01 22:03:44 -0600

    Implementing concurrent processes

--- Assignment1/p1.c
+++ Assignment1/p1.c
@@ -5,114 +5,98 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define buffer_max 32;
-
-/*Extracting tokens from terminal input*/
-char **extract_token(char *line){
-    /*Declare Variables*/
-    int buffer = buffer_max;
-    int i = 0;                                                      //Counter
-    char *delim = " \t\r\n";                                        //Delimiter for strtok
-    
-    char **tokens = malloc(buffer * sizeof(char*));
-    char *token;
-    
-    if(tokens == NULL){
-        fprintf(stderr, "Failed to allocate memory");
-        exit(1);
-    }
-
-    token = strtok(line, delim);
-    while(token != NULL){                                           
-        //Grab the token and copy it to an array
-        tokens[i] = token;
-        i++;
-
-        if(i >= buffer){
-            buffer += buffer;
-            tokens = realloc(tokens, buffer * sizeof(char*));
-            //Print statement if fail to allocate memory
-            if(tokens == NULL){
-                fprintf(stderr, "Fail to allocate memory");
-                exit(1);
-            }
-        }
-        token = strtok(NULL, delim);
-    }
-    //Null terminated the string for execvp;
-    tokens[i] = NULL;
-
-    return tokens;
-}
-
 /*Command Argument Line*/
-void process_exec(char **args){
+void process_exec(char *args[100][100], int process_num){
     /*Variables*/
     int status;
-    //int cexit_status;
     int exit_status;
-
+    int i = 0;
+    int j = 0;
     char *exit_name = "exit";
+    pid_t pids[100];
+    pid_t wpid;
     
-    if(strcmp(args[0], exit_name) == 0){
-        exit(0);
-        return;
-    }
 
-    pid_t rc  = fork();
-
-    if(rc < 0){
-        //fork failed
-        fprintf(stderr, "Fail to create process\n");
-        exit(1);
-    }
-    else if(rc == 0){
-        //printf("Executing Process ID: %d\n", (int) getpid());
-        if(execvp(args[0], args) == -1){
-            //cexit_status = 127;
-            //printf("jsh status: %d\n", cexit_status);
-            exit(127);            
+    //Built-in Commands -- Check for Exit
+    for(j = 0; j <= process_num; j++){
+        if(strcmp(args[j][0], exit_name) == 0){
+            exit(0);
+            return;
         }
     }
-    else{
-        waitpid(rc, &status, 0);
 
+    //Child Process
+    for(i =0; i <= process_num; i++){
+        pids[i] = fork();
+        if(pids[i] < 0){
+            //fork failed
+            fprintf(stderr, "Fail to create process\n");
+            exit(1);
+        }
+        else if(pids[i] == 0){
+            if(execvp(args[i][0], args[i]) == -1){
+                exit(127);
+            }
+        }
+    }
+
+    //Parent Process Waiting for all Child processes
+    while((wpid = waitpid(-1, &status, 0)) != -1){
         if(WIFEXITED(status)){
             exit_status = WEXITSTATUS(status);
             printf("jsh status: %d\n", exit_status);
-        }       
-    }    
+        }   
+    }
 }
 
 /*Main Function*/
 int main(int argc, char const *argv[])
 {
+    
     /*Set up void variables*/
     (void)argc;
     (void)argv;
+    int i, j;
+    char *delim = " \t\r\n";                                        //Delimiter for strtok
+    //char *pip_char = "|";
+
+    char *tokens[100][100];
+    char *token;
+    /*Variables for prompt*/
+    char *prompt = "jsh$";    
+    char line[100];   
 
     /*Main Loop*/    
-    do{
-        /*Variables*/
-        char *prompt = "jsh$";    
-        char *line = NULL;
-        char **tokens;
-        size_t size = 0;
-        
+    while(1){
+        /*Variables for tokens*/
+        i = 0;
+        j = 0;
+
         /*Get Input from the user*/
         printf("%s ", prompt);
-        getline(&line ,&size, stdin);
+        fgets(line, sizeof(line), stdin);
+               
+        /*Extracting the tokens*/
+        token = strtok(line, delim);
+            
+        while(token != NULL){
+            tokens[i][j] = token;
+
+            if(strcmp(tokens[i][j], "|") == 0){
+                tokens[i][j] = NULL;
+                i = i + 1;
+                j = 0;
+                token = strtok(NULL, delim);
+            }
+            else{
+                j++;
+                token = strtok(NULL, delim);
+            }                     
+        }
+        tokens[i][j] = NULL;
+        process_exec(tokens, i);
         
-        // /*Call a function split the input into arguments */
-        tokens = extract_token(line);        
-        process_exec(tokens);
-
-        //printf();
-        free(line);
-        free(tokens);
-
     }
-    while(1);
 
     return 0;
 }
